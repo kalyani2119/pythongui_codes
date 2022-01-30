3138f5cd59ee45c3394bceb79d94e7d662786387
KALYANI S <91209741+kalyani2119@users.noreply.github.com>
2022-01-30 15:42:57 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+from cProfile import label
+from tkinter import *
+root = Tk()
+# Creating a label widget
+myLabel = Label(root, text="Hello World!")
+#showing it on to the screen
+myLabel.pack()
+root.mainloop()
